8e14d72240b507ff3408385dfa79f5e7c993a4f2
NUG516 <cdm05162@naver,com>
2023-11-28 17:06:39 +0900

    LED_control

--- LED_control/LED_control/main.c
+++ LED_control/LED_control/main.c
@@ -10,7 +10,8 @@
 #define BLUE 0x08	// D11, PB3
 #define input_value PINB	// read only register
 
-unsigned int count = 0;	// 0~65,536
+// unsigned int count = 0;	// 0~65,536
+unsigned long count = 0;
 unsigned char pattern_table[2] = {LED | RED | BLUE, GRENN | BLUE};
 unsigned char index = 0;	// 배열 요소 접근 변수
 	
@@ -19,18 +20,19 @@ void setup(){
 	PORTB = 0x00;
 }
 void loop(){
-	PORTB |= 0xFF;
+	PORTB = 0xFF;
 	PORTB &= ~(pattern_table[(index++) & 0x01]);	// (index++)&0x배열의 개수(16진수), 순차적으로 인덱스의 요소만 and 연산으로 나옴
 	PORTB &= ~(BTN);	// BTN만 0으로 바꾸기
+	
 	// for(count = 0; count < 10000; count++);	// AVR 시간 측정(발진기의 펄스이용), 0x2710 == 10,000(10진수), 10ms
 	if((input_value & BTN) == BTN){
-		_delay_ms(100);
+		for(count=0; count < 1000000; count++){
 			if((input_value & BTN) == BTN){		// BTN 누를 시
 				PORTB = 0x00;
 				while((input_value & BTN));	// BTN 지속, 떼기 전까지
+			}
 		}
 	}
-	_delay_ms(2000);
 	/*PORTB = pattern_table[1];
 	_delay_ms(2000);*/
 	
