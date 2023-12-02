3c554e69ced051b2a65dc7e8449b81756902e62a
NGU516 <cdm05162@naver.com>
2023-12-02 17:53:32 +0900

    serial_2

--- Serial_2/main.c
+++ Serial_2/main.c
@@ -1,11 +1,17 @@
+#ifndef F_CPU
 #define F_CPU 16000000UL
+#endif
 #include <avr/io.h>
 
 #define BTN 0x01
+#define RED 0x02	// D9, PB1
+#define GREEN 0x04	// D10, PB2
+#define BLUE 0x08	// D11, PB3
+#define LED 0x20    // D13, PB5
 #define input_value PINB
 
 void setup() {
-	DDRB = 0x20;    
+	DDRB = 0xFF;    
 	PORTB = 0x00;   
 
 	DDRD = 0x02;    // PD0 = RXD 로 입력 설정, PD1 = TXD 로 출력 설정
@@ -26,6 +32,11 @@ void USART0_send(unsigned char Data){
 	while(!( UCSR0A & (1<<UDRE0)) );
 	while(!(UCSR0A & 0x20));
 	UDR0 = Data;
+}
+
+unsigned char USART0_read(){
+    while(!( UCSR0A & (1<<RXC0)) );
+    return UDR0;    // 수신된 데이터 리턴
 }
 
 void Timer1_init() {
@@ -71,23 +82,30 @@ int main(){
 	Timer1_1s_delay();
 	
 	while(1){
-		Data = 'A';
+        PORTB = 0x00;   // 다 끄기
+		Data = USART0_read();   // 데이터 수신
 
-		PORTB = 0x00; // LED On
-
-        if((input_value & BTN) == BTN){
-            USART0_send(Data);
-
-            Timer1_1s_delay();
-
-			if((input_value & BTN) == BTN){		// BTN 누를 시
-			    while((input_value & BTN)){         // BTN 지속, 떼기 전까지    
-                    PORTB = 0x20;   // LED ON
-                    USART0_send(Data);
-                    Timer1_100ms_delay();    
-                }	
-		    }
-	    }
+		switch(Data){
+            // A를 입력 받으면 빨강
+            case 'A':   PORTB |= RED;
+                PORTB |=  LED;
+                Timer1_1s_delay();
+                break;
+            // B를 입력 받으면 파랑
+            case 'B':   PORTB |= BLUE;
+                PORTB |=  LED;
+                Timer1_1s_delay();
+                break;
+            // C를 입력 받으면 초록
+            case 'C': PORTB |= GREEN;
+                PORTB |= LED;
+                Timer1_1s_delay();
+                break;
+            default:    USART0_send('D');   // 입력 오류 전송
+                continue;
+                break;
+        }
+        USART0_send('E');   // 제어 완료 전송
 	}
 
 }
