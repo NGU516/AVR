9f12112d250f130bfa5fea5061198e5dd050bfe4
dong min choi <cdm05162@naver.com>
2023-12-26 12:12:55 +0900

    Tilt_Sensor

---
+++ Tilt_sensor/Tilt_sensor/main.c
@@ -0,0 +1,18 @@
+/*
+ * Tilt_sensor.c
+ *
+ * Created: 2023-12-26 오전 11:50:03
+ * Author : cdm05
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
